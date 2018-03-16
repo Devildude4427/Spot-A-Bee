cc808cecfe31a9cfd580c073e39bf53b94dbb300
Ryan Christian <ryanchristian4427@gmail.com>
2018-03-16 00:57:37 +0000

    Completely working map page with permissions and marker updates

--- app/src/main/java/assignment/com/spotabee/MapsActivity.java
+++ app/src/main/java/assignment/com/spotabee/MapsActivity.java
@@ -1,10 +1,18 @@
 package assignment.com.spotabee;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,10 +21,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity
+        implements OnMapReadyCallback {
 
     private static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 0;
-    private GoogleMap mMap;
+    private GoogleMap googleMap;
+    private LocationManager locationManager =null;
+    private LocationListener locationListener=null;
+
+    private static final String TAG = "Debug";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager)
+                getSystemService(Context.LOCATION_SERVICE);
+    }
+
+
+
+    public void checkIfPermissionsGiven() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationListener = new MapsActivity.MyLocationListener();
+
+            locationManager.requestLocationUpdates(LocationManager
+                    .GPS_PROVIDER, 5000, 10,locationListener);
+        } else {
+            requestLocationPermission();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+        checkIfPermissionsGiven();
+    }
+
+    private void requestLocationPermission() {
+        // Permission has not been granted and must be requested.
+        // Request the permission. The result will be received in onRequestPermissionResult().
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
     }
 
     @Override
@@ -37,19 +82,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationListener = new MapsActivity.MyLocationListener();
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
+                    locationManager.requestLocationUpdates(LocationManager
+                            .GPS_PROVIDER, 5000, 10,locationListener);
 
                 } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-
-                    LatLng cardiff = new LatLng(51.481581, -3.179090);
-                    mMap.addMarker(new MarkerOptions().position(cardiff)
-                            .title("Marker in Cardiff"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(cardiff));
-
+                    setUpMap(51.481581, -3.179090);
                 }
                 return;
             }
@@ -59,26 +98,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            //Get Coordinates and plot
-        } else {
-            requestLocationPermission();
-        }
+    public void setUpMap(Double Latitude, Double Longitude) {
+        LatLng newMarker = new LatLng(Latitude, Longitude);
+        googleMap.addMarker(new MarkerOptions().position(newMarker)
+                .title("Maker"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(newMarker));
     }
 
+    //Listener class to get longitude and latitude
+    private class MyLocationListener implements LocationListener {
 
+        @Override
+        public void onLocationChanged(Location loc) {
+            Toast.makeText(getBaseContext(),"Location changed : Lat: " +
+                            loc.getLatitude()+ " Lng: " + loc.getLongitude(),
+                    Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " +loc.getLongitude();
+            Log.v(TAG, longitude);
+            String latitude = "Latitude: " +loc.getLatitude();
+            Log.v(TAG, latitude);
 
+            setUpMap(loc.getLatitude(), loc.getLongitude());
+        }
 
-    private void requestLocationPermission() {
-        // Permission has not been granted and must be requested.
-            // Request the permission. The result will be received in onRequestPermissionResult().
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
     }
 
 
