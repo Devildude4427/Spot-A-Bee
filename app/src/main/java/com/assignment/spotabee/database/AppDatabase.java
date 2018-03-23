54028bc724c29f3c94490e846abdfc3a6a41a7ae
Lauren Heymer <laurenheymer@gmail.com>
2018-03-23 13:10:39 +0000

    Changed package structure so that there is no more reference to assignment.com.spotabee. The database package is now in com.assignment.spotabee

--- app/src/main/java/com/assignment/spotabee/database/AppDatabase.java
+++ app/src/main/java/com/assignment/spotabee/database/AppDatabase.java
@@ -1,9 +1,12 @@
-package assignment.com.spotabee.database;
+package com.assignment.spotabee.database;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import com.assignment.spotabee.DescriptionForm;
+
+import com.assignment.spotabee.database.Description;
+import com.assignment.spotabee.database.DescriptionDao;
 
 @Database(entities = {Description.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
