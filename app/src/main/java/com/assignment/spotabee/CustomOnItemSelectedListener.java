19dabfffb91e6e9507b034e1a10eb5018391e6bf
Lauren Heymer <laurenheymer@gmail.com>
2018-04-05 11:52:17 +0100

    Spinner now contains a dynamic list of address lines based on geolocation search results created by the user. The location field of the description form is filled in depending on which option the user chooses.

--- app/src/main/java/com/assignment/spotabee/CustomOnItemSelectedListener.java
+++ app/src/main/java/com/assignment/spotabee/CustomOnItemSelectedListener.java
@@ -2,6 +2,7 @@ package com.assignment.spotabee;
 
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 
@@ -9,6 +10,9 @@ import android.widget.Toast;
 public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+//        EditText location = view.findViewById(R.id.locationField);
+//        location.setText(parent.getSelectedItemPosition());
+
         Toast.makeText(parent.getContext(),
                 "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
                 Toast.LENGTH_SHORT).show();
