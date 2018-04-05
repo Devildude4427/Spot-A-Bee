a8f99588b5026673c169c5f40b8e15a640e6b87b
Lauren Heymer <laurenheymer@gmail.com>
2018-04-05 11:09:17 +0100

    Geolocation from user input is able to retrieve a list of maximum results. Very early stages. Just logs results. Spinner in progress to display results.

---
+++ app/src/main/java/com/assignment/spotabee/CustomOnItemSelectedListener.java
@@ -0,0 +1,22 @@
+package com.assignment.spotabee;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+
+// Taken from https://www.mkyong.com/android/android-spinner-drop-down-list-example/
+public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        Toast.makeText(parent.getContext(),
+                "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
+}
