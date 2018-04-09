e6037b8ff258010277d2171bf5f0611bb09d3288
Zoila Garman <c1452589@cardiff.ac.uk>
2018-04-09 19:08:11 +0100

    This is a copy of the project up to the 9th of April 2018, as a safe copy available for everyone, in case of faults. Please notice that another copy is keep in the developer;s device as a precaution for further failures. Please contact developer Zoila Garman for more details

--- app/src/main/java/com/assignment/spotabee/AboutUs.java
+++ app/src/main/java/com/assignment/spotabee/AboutUs.java
@@ -1,8 +1,35 @@
 package com.assignment.spotabee;
 
-/**
- * Created by Ryun on 3/18/2018.
- */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class AboutUs {
+
+public class AboutUs extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+
+        Button advanceToQuestionTwo = (Button) findViewById(R.id.button2);
+        advanceToQuestionTwo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(AboutUs.this, Home.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+    }
 }
--- app/src/main/java/com/assignment/spotabee/Markers.java
+++ app/src/main/java/com/assignment/spotabee/Markers.java
@@ -8,9 +8,9 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
