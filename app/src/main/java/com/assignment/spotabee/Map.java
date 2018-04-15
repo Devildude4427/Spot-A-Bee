7da70468336a67e89bff92719a0ad6dcadf287bc
Ryan Christian <christianr@cardiff.ac.uk>
2018-04-15 18:32:08 +0100

    Moving all permissions to Home.java, and cleaning Home as well

--- app/src/main/java/com/assignment/spotabee/Map.java
+++ app/src/main/java/com/assignment/spotabee/Map.java
@@ -1,18 +1,16 @@
 package com.assignment.spotabee;
 
+
 import android.Manifest;
-import android.accounts.AccountManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
@@ -28,21 +26,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+
 public class Map extends FragmentActivity
         implements OnMapReadyCallback {
 
-//    private static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION_AND_ACCOUNTS = 0;
-    private static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 1;
-//    private static final int PERMISSION_REQUEST_ACCESS_ACCOUNT_DETAILS = 2;
-//    private static final int CHOOSE_ACCOUNT = 99;
     private static final String CHANNEL_ID = "One";
-    private GoogleMap googleMap;
-    private LocationManager locationManager;
-    private LocationListener locationListener;
-    private AccountManager accountManager;
-    private NotificationManager notificationManager;
-
     private static final String TAG = "Debug";
+    private static GoogleMap googleMap;
+    private NotificationManager notificationManager;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,25 +50,38 @@ public class Map extends FragmentActivity
         locationManager = (LocationManager)
                 getSystemService(Context.LOCATION_SERVICE);
 
-        //        accountManager = (AccountManager)
-        //           getSystemService(Context.ACCOUNT_SERVICE);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
 
-        initChannels();
+            locationListener = new Map.MyLocationListener();
+            locationManager.requestLocationUpdates(LocationManager
+                    .GPS_PROVIDER, 5000, 10, locationListener);
 
+        } else {
+            setUpMap(51.481581, -3.179090);
+        }
+
+        initChannel();
+
+        /*Builder for notifications*/
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.camera_button)
                 .setContentTitle("Spot-A-Bee")
                 .setContentText("This is a notification")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
+        /*Way to call a notification*/
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
 
         // notificationId is a unique int for each notification
         notificationManager.notify(1, mBuilder.build());
     }
 
-    /*Creates the channels for notifications to use*/
-    public void initChannels() {
+    /**
+     * Creates the Channel for the notification to be used
+     */
+    public void initChannel() {
         if (Build.VERSION.SDK_INT < 26) {
             return;
         }
@@ -87,122 +94,19 @@ public class Map extends FragmentActivity
         notificationManager.createNotificationChannel(channel);
     }
 
+    /**
+     * Creates the map that is going to be used
+     *
+     * @param googleMap GoogleMap object
+     */
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
-        checkIfPermissionsGiven();
     }
 
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    public void checkIfPermissionsGiven() {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-//                != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this,
-//                        Manifest.permission.GET_ACCOUNTS) != PackageManager.PERMISSION_GRANTED) {
-//            requestLocationAccountPermission(); }
-           if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestLocationPermission(); }
-//        else {
-//            requestAccountPermission();
-//        }
-
-        try {
-            locationListener = new Map.MyLocationListener();
-            locationManager.requestLocationUpdates(LocationManager
-                    .GPS_PROVIDER, 5000, 10,locationListener);
-//            Intent intent = accountManager.newChooseAccountIntent(null, null, new String[]{"com.google"}, null, null, null,
-//                    null);
-//            startActivityForResult(intent, CHOOSE_ACCOUNT);
-        } catch (Exception e) {
-            Log.d(TAG, "Exception" + e);
-        }
-    }
-
-    /*Has the user select a Google account that this app can then see and use*/
-//    protected void onActivityResult(final int requestCode, final int resultCode,
-//                                    final Intent data){
-//        if (requestCode == CHOOSE_ACCOUNT && resultCode == RESULT_OK) {
-//            String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-//            Log.v(TAG, accountName);
-//        } else {
-//            Log.v(TAG, "There was an error in the account picker");
-//        }
-//    }
-
-    /*Method for requesting permission to both Location and Account Permissions*/
-//    private void requestLocationAccountPermission() {
-//        // Permission has not been granted and must be requested.
-//        // Request the permission. The result will be received in onRequestPermissionResult().
-//        ActivityCompat.requestPermissions(this,
-//                new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-//                        Manifest.permission.GET_ACCOUNTS},
-//                PERMISSION_REQUEST_ACCESS_FINE_LOCATION_AND_ACCOUNTS);
-//    }
-
-    /*Method for requesting permission to Location Permission*/
-    private void requestLocationPermission() {
-        // Permission has not been granted and must be requested.
-        // Request the permission. The result will be received in onRequestPermissionResult().
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                PERMISSION_REQUEST_ACCESS_FINE_LOCATION);
-    }
-
-    /*Method for requesting permission to Account Permission*/
-//    private void requestAccountPermission() {
-//        // Permission has not been granted and must be requested.
-//        // Request the permission. The result will be received in onRequestPermissionResult().
-//        ActivityCompat.requestPermissions(this,
-//                new String[]{Manifest.permission.GET_ACCOUNTS},
-//                PERMISSION_REQUEST_ACCESS_ACCOUNT_DETAILS);
-//    }
-
-
-    /*Method handles what happens after account permissions are requested*/
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[],
-                                           int[] grantResults) {
-        switch (requestCode) {
-//            case PERMISSION_REQUEST_ACCESS_FINE_LOCATION_AND_ACCOUNTS: {
-//                // If request is cancelled, the result arrays are empty.
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    checkIfPermissionsGiven();
-//                } else {
-//                    setUpMap(51.481581, -3.179090);
-//                    Log.v(TAG, "User needs to make an account");
-//                }
-//                return;
-//            }
-            case PERMISSION_REQUEST_ACCESS_FINE_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                } else {
-                    setUpMap(51.481581, -3.179090);
-                }
-                return;
-            }
-//            case PERMISSION_REQUEST_ACCESS_ACCOUNT_DETAILS: {
-//                // If request is cancelled, the result arrays are empty.
-//                if (grantResults.length > 0
-//                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                } else {
-//                    //Redirect to force user to create an account
-//                    Log.v(TAG, "User needs to make an account");
-//                }
-//            }
-            // other 'case' lines to check for other
-            // permissions this app might request.
-        }
-    }
-
-    public void setUpMap(Double Latitude, Double Longitude) {
+    /*Method to create a marker at positions*/
+    public static void setUpMap(Double Latitude, Double Longitude) {
         LatLng newMarker = new LatLng(Latitude, Longitude);
         googleMap.addMarker(new MarkerOptions().position(newMarker)
                 .title("Marker"));
@@ -210,33 +114,33 @@ public class Map extends FragmentActivity
     }
 
     //Listener class to get longitude and latitude
-    private class MyLocationListener implements LocationListener {
+    public class MyLocationListener implements LocationListener {
 
         @Override
         public void onLocationChanged(Location loc) {
-            Toast.makeText(getBaseContext(),"Location changed : Lat: " +
-                            loc.getLatitude()+ " Lng: " + loc.getLongitude(),
+            Toast.makeText(getBaseContext(),"Location changed : Lat: "
+                            + loc.getLatitude() + " Lng: " + loc.getLongitude(),
                     Toast.LENGTH_SHORT).show();
-            String longitude = "Longitude: " +loc.getLongitude();
+            String longitude = "Longitude: " + loc.getLongitude();
             Log.v(TAG, longitude);
-            String latitude = "Latitude: " +loc.getLatitude();
+            String latitude = "Latitude: " + loc.getLatitude();
             Log.v(TAG, latitude);
 
             setUpMap(loc.getLatitude(), loc.getLongitude());
         }
 
         @Override
-        public void onProviderDisabled(String provider) {
+        public void onProviderDisabled(final String provider) {
             // TODO Auto-generated method stub
         }
 
         @Override
-        public void onProviderEnabled(String provider) {
+        public void onProviderEnabled(final String provider) {
             // TODO Auto-generated method stub
         }
 
         @Override
-        public void onStatusChanged(String provider,
+        public void onStatusChanged(final String provider,
                                     int status, Bundle extras) {
             // TODO Auto-generated method stub
         }
