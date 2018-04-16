97d336350d19763caf4c8c40d0afa596e8c71bdb
Ryan Christian <ryanchristian4427@gmail.com>
2018-04-16 10:45:03 +0100

    Altered Map.java to better handle situations in which it does not have proper permissions

--- app/src/main/java/com/assignment/spotabee/Home.java
+++ app/src/main/java/com/assignment/spotabee/Home.java
@@ -30,8 +30,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 
-import static com.assignment.spotabee.Map.setUpMap;
-
 //import com.assignment.spotabee.Home.R;
 
 public class Home extends AppCompatActivity implements View.OnClickListener {
