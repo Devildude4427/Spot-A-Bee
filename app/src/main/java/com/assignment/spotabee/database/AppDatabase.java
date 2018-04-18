df7c7797f3b66f76157dff4751d2fc282bb4b92b
Ryan Christian <christianr@cardiff.ac.uk>
2018-04-18 22:55:30 +0100

    Fixed all issues with Markers as a fragment, streamlined database retrieval

--- app/src/main/java/com/assignment/spotabee/database/AppDatabase.java
+++ app/src/main/java/com/assignment/spotabee/database/AppDatabase.java
@@ -3,11 +3,6 @@ package com.assignment.spotabee.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-import com.assignment.spotabee.DescriptionForm;
-
-import com.assignment.spotabee.database.Description;
-import com.assignment.spotabee.database.DescriptionDao;
-
 @Database(entities = {Description.class}, version = 3)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract DescriptionDao descriptionDao();
