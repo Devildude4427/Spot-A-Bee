29449bd3ed34122d3dfe80ca7a3985f3069bc8ca
Lauren Heymer <laurenheymer@gmail.com>
2018-04-23 11:13:30 +0100

    Addition of business logic required for image recognition. Not yet integrated into the app

---
+++ app/src/main/java/com/assignment/spotabee/imagerecognition/FlowerIdentificationModel.java
@@ -0,0 +1,53 @@
+package com.assignment.spotabee.imagerecognition;
+
+import java.io.File;
+
+/**
+ * Created by Lauren on 4/22/2018.
+ * Stores file/ byte[] that will be used in a Clarifai request
+ * to identify an image and the String name that is returned from
+ * such request
+ */
+
+public class FlowerIdentificationModel {
+    private File imageFileToIdentify;
+    private byte[] imageBytesToIdentify;
+    private String identifiedName;
+
+
+    // Overloaded constructor to allow use of different data sources
+    public FlowerIdentificationModel(File imageFileToIdentify) {
+        this.imageFileToIdentify = imageFileToIdentify;
+        this.identifiedName = null;
+    }
+
+    public FlowerIdentificationModel(byte[] imageBytesToIdentify) {
+        this.imageBytesToIdentify = imageBytesToIdentify;
+        this.identifiedName = null;
+    }
+
+
+    public File getImageFileToIdentify() {
+        return imageFileToIdentify;
+    }
+
+    public byte[] getImageBytesToIdentify() {
+        return imageBytesToIdentify;
+    }
+
+    public String getIdentifiedName() {
+        return identifiedName;
+    }
+
+    public void setImageFileToIdentify(File imageFileToIdentify) {
+        this.imageFileToIdentify = imageFileToIdentify;
+    }
+
+    public void setImageBytesToIdentify(byte[] imageBytesToIdentify) {
+        this.imageBytesToIdentify = imageBytesToIdentify;
+    }
+
+    public void setIdentifiedName(String identifiedName) {
+        this.identifiedName = identifiedName;
+    }
+}
