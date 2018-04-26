9c7ddbdffde5ae6aeafd53dc5fa56b70ab14fe3c
Lauren Heymer <c1769948@cardiff.ac.uk>
2018-04-26 14:40:20 +0100

    Addition of download manager and dynamic receiver to download a guide on helping preserve the bee species. A new fragment has been created for this and a user can click on a an image view of they would like to download the PDF. Yet to add a loading spinner to show that the app has not crashed whilst performing the download. Also have not created a notification item yet to allow the user to open the guide

---
+++ app/src/main/java/com/assignment/spotabee/FragmentDownloadPdfGuide.java
@@ -0,0 +1,231 @@
+package com.assignment.spotabee;
+
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.content.Context.DOWNLOAD_SERVICE;
+
+public class FragmentDownloadPdfGuide extends Fragment implements View.OnClickListener{
+    private DownloadManager downloadManager;
+    private ImageView downloadPdf;
+    private long downloadReference;
+    private Context appContext;
+    private BroadcastReceiver downloadReceiver;
+    private View rootView;
+    private Uri uri;
+    private View v;
+
+    public FragmentDownloadPdfGuide() {
+        // Required empty public constructor
+    }
+
+
+    public static FragmentDownloadPdfGuide newInstance(String param1, String param2) {
+        FragmentDownloadPdfGuide fragment = new FragmentDownloadPdfGuide();
+        Bundle args = new Bundle();
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_fragment_download_pdf_guide, container, false);
+
+        downloadManager = (DownloadManager) getActivity().getSystemService(DOWNLOAD_SERVICE);
+
+        downloadPdf = rootView.findViewById(R.id.download_pdf_bee);
+        downloadPdf.setOnClickListener(this);
+        appContext = getActivity().getApplicationContext();
+
+        downloadReceiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+                //check if the broadcast message is for our enqueued download
+                long referenceId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+
+                if(referenceId == downloadReference) {
+
+                    Toast toast = Toast.makeText(getActivity(),
+                            "Guide download complete", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP, 25, 400);
+                    toast.show();
+                }
+
+            }
+        };
+
+        IntentFilter filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        getActivity().registerReceiver(downloadReceiver, filter);
+
+        return rootView;
+    }
+
+    private long downloadData (Uri uri) {
+        this.uri = uri;
+        this.v = v;
+
+        downloadManager = (DownloadManager) getActivity().getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setTitle("Help the Bees");
+        request.setDescription("Guide on helping to preserve the bee population");
+
+
+        request.setDestinationInExternalFilesDir(getActivity(),
+                Environment.DIRECTORY_DOWNLOADS,"Bee_Guide.pdf");
+
+
+        //Enqueue download and save into referenceId
+        downloadReference = downloadManager.enqueue(request);
+
+        check_Image_Status(downloadReference);
+        return downloadReference;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id){
+            case R.id.download_pdf_bee:
+                Uri uriForPdf = Uri.parse("https://friendsoftheearth.uk/sites/default/files/downloads/bees_booklet.pdf");
+                downloadData(uriForPdf);
+                break;
+        }
+    }
+
+    private void check_Image_Status(long downloadReference) {
+
+        DownloadManager.Query pdfDownloadQuery = new DownloadManager.Query();
+        //set the query filter to our previously Enqueued download
+        pdfDownloadQuery.setFilterById(downloadReference);
+
+        //Query the download manager about downloads that have been requested.
+        Cursor cursor = downloadManager.query(pdfDownloadQuery);
+        if(cursor.moveToFirst()){
+            downloadStatus(cursor, downloadReference);
+        }
+    }
+
+    private void downloadStatus(Cursor cursor, long downloadId){
+
+        //column for download  status
+        int columnIndex = cursor.getColumnIndex(DownloadManager.COLUMN_STATUS);
+        int status = cursor.getInt(columnIndex);
+        //column for reason code if the download failed or paused
+        int columnReason = cursor.getColumnIndex(DownloadManager.COLUMN_REASON);
+        int reason = cursor.getInt(columnReason);
+
+        String statusText = "";
+        String reasonText = "";
+
+        switch(status){
+            case DownloadManager.STATUS_FAILED:
+                statusText = "STATUS_FAILED";
+                switch(reason){
+                    case DownloadManager.ERROR_CANNOT_RESUME:
+                        reasonText = "ERROR_CANNOT_RESUME";
+                        break;
+                    case DownloadManager.ERROR_DEVICE_NOT_FOUND:
+                        reasonText = "ERROR_DEVICE_NOT_FOUND";
+                        break;
+                    case DownloadManager.ERROR_FILE_ALREADY_EXISTS:
+                        reasonText = "ERROR_FILE_ALREADY_EXISTS";
+                        break;
+                    case DownloadManager.ERROR_FILE_ERROR:
+                        reasonText = "ERROR_FILE_ERROR";
+                        break;
+                    case DownloadManager.ERROR_HTTP_DATA_ERROR:
+                        reasonText = "ERROR_HTTP_DATA_ERROR";
+                        break;
+                    case DownloadManager.ERROR_INSUFFICIENT_SPACE:
+                        reasonText = "ERROR_INSUFFICIENT_SPACE";
+                        break;
+                    case DownloadManager.ERROR_TOO_MANY_REDIRECTS:
+                        reasonText = "ERROR_TOO_MANY_REDIRECTS";
+                        break;
+                    case DownloadManager.ERROR_UNHANDLED_HTTP_CODE:
+                        reasonText = "ERROR_UNHANDLED_HTTP_CODE";
+                        break;
+                    case DownloadManager.ERROR_UNKNOWN:
+                        reasonText = "ERROR_UNKNOWN";
+                        break;
+                }
+                break;
+            case DownloadManager.STATUS_PAUSED:
+                statusText = "STATUS_PAUSED";
+                switch(reason){
+                    case DownloadManager.PAUSED_QUEUED_FOR_WIFI:
+                        reasonText = "PAUSED_QUEUED_FOR_WIFI";
+                        break;
+                    case DownloadManager.PAUSED_UNKNOWN:
+                        reasonText = "PAUSED_UNKNOWN";
+                        break;
+                    case DownloadManager.PAUSED_WAITING_FOR_NETWORK:
+                        reasonText = "PAUSED_WAITING_FOR_NETWORK";
+                        break;
+                    case DownloadManager.PAUSED_WAITING_TO_RETRY:
+                        reasonText = "PAUSED_WAITING_TO_RETRY";
+                        break;
+                }
+                break;
+            case DownloadManager.STATUS_PENDING:
+                statusText = "STATUS_PENDING";
+                break;
+            case DownloadManager.STATUS_RUNNING:
+                statusText = "STATUS_RUNNING";
+                break;
+            case DownloadManager.STATUS_SUCCESSFUL:
+                statusText = "STATUS_SUCCESSFUL";
+                reasonText = "Successfully downloaded";
+                break;
+        }
+
+        if(downloadId == downloadReference) {
+
+            Toast toast = Toast.makeText(getActivity(),
+                    "PDF Guide Status:" + "\n" + statusText + reasonText,
+                    Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.TOP, 25, 400);
+            toast.show();
+
+        }
+
+        // Make a delay of 3 seconds so that next toast (Music Status) will not merge with this one.
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            }
+        }, 3000);
+    }
+
+}
