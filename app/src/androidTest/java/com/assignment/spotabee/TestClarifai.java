18af5c17986b014e70e33bc0a492ec01d8cc9ab9
Lauren Heymer <c1769948@cardiff.ac.uk>
2018-04-26 11:09:57 +0100

    Working test for Clarifai business logic

--- app/src/androidTest/java/com/assignment/spotabee/TestClarifai.java
+++ app/src/androidTest/java/com/assignment/spotabee/TestClarifai.java
@@ -1,5 +1,10 @@
 package com.assignment.spotabee;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.test.InstrumentationRegistry;
+
 import com.assignment.spotabee.imagerecognition.ClarifaiClientGenerator;
 import com.assignment.spotabee.imagerecognition.ClarifaiRequest;
 import com.assignment.spotabee.imagerecognition.FlowerIdentificationModel;
@@ -7,8 +12,13 @@ import com.assignment.spotabee.imagerecognition.FlowerIdentificationModel;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import clarifai2.api.ClarifaiClient;
@@ -26,21 +36,57 @@ public class TestClarifai {
     private final ClarifaiClient client = ClarifaiClientGenerator.generate("d984d2d494394104bb4bee0b8149523d");
     private FlowerIdentificationModel flowerIdentificationModel;
     private ClarifaiRequest clarifaiRequest;
+    private Context context;
 
+    @Before
+    public void initialise() {
+        try {
+            context = InstrumentationRegistry.getInstrumentation().getContext();
+            InputStream inputSteamOfImage = context.getAssets().open("testimage/comparativeSunflower.jpg");
+            byte[] testImageBytes = getBitMap(inputSteamOfImage);
 
-    // Commented out because Android Studio can't find the input file
-//    @Before
-//    public void initialise() {
-//
-//
-//        flowerIdentificationModel = new FlowerIdentificationModel(new File("src/androidTest/res/comparativeSunflower.jpg"));
-//
-////      ?  clarifaiRequest = new ClarifaiRequest(client, "flower_species", flowerIdentificationModel.getImageFileToIdentify());
-//
-//    }
-//
-//    @Test
-//    public void testCorrectIdentification() {
-//        assertEquals("sunflower", clarifaiRequest.executRequest());
-//    }
+            clarifaiRequest = new ClarifaiRequest(client, "flower_species", testImageBytes);
+        } catch (IOException e){
+            LOGGER.log(Level.SEVERE, "IOException occurred when retrieving image file");
+        }
+
+    }
+
+    public byte[] getBitMap(InputStream inputStream){
+
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(inputStream);
+            final ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+            return outStream.toByteArray();
+
+        } catch (Exception e) {
+            return null;
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException ignored) {
+                }
+            }
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
+        }
+    }
+
+    @Test
+    public void testCorrectIdentification() {
+        try {
+            context = InstrumentationRegistry.getInstrumentation().getContext();
+            InputStream inputSteamOfImage = context.getAssets().open("testimages/comparativeSunflower.jpg");
+            byte[] testImageBytes = getBitMap(inputSteamOfImage);
+
+            clarifaiRequest = new ClarifaiRequest(client, "flower_species", testImageBytes);
+            assertEquals("sunflower", clarifaiRequest.executRequest());
+        } catch (IOException e){
+            LOGGER.log(Level.SEVERE, "IOException occurred when retrieving image file");
+        }
+    }
 }
