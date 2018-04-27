2654a69350c2b52fe1d0d7124be0a7f969c8de4d
Lauren Heymer <c1769948@cardiff.ac.uk>
2018-04-27 18:15:11 +0100

    Addition of link to Spot a Bee YouTube video to AboutUs

--- app/src/main/java/com/assignment/spotabee/fragments/FragmentAboutUs.java
+++ app/src/main/java/com/assignment/spotabee/fragments/FragmentAboutUs.java
@@ -1,10 +1,12 @@
 package com.assignment.spotabee.fragments;
 
+import android.net.Uri;
 import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,13 +18,28 @@ import com.assignment.spotabee.R;
 
 
 public class FragmentAboutUs extends Fragment {
+    private AppCompatButton goToYouTube;
+    private View rootView;
+    private static final String videoURL = "https://www.youtube.com/watch?v=pbeSHt4B3hg";
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_menu_aboutus, container, false);
+
+        rootView = inflater.inflate(R.layout.fragment_menu_aboutus, container, false);
+        this.goToYouTube = rootView.findViewById(R.id.goToYoutube);
+        goToYouTube.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri videoUri =  Uri.parse(videoURL);
+                Intent youtubeVideo = new Intent(Intent.ACTION_VIEW);
+                youtubeVideo.setData(videoUri);
+                startActivity(youtubeVideo);
+            }
+        });
+        return rootView;
     }
 
     @Override
