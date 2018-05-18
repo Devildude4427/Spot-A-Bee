817a45407a646ee2a669ab7e1c5d7570d6041f7c
Lauren Heymer <c1769948@cardiff.ac.uk>
2018-05-18 10:07:58 +0100

    Have finished making all screens scrollable and compatible with a landscape orientation

--- app/src/androidTest/java/com/assignment/spotabee/TestDescriptionFormLayout.java
+++ app/src/androidTest/java/com/assignment/spotabee/TestDescriptionFormLayout.java
@@ -30,6 +30,7 @@ import java.lang.reflect.Method;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
