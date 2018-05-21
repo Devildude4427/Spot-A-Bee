15e5a7de9002489efcb99551bf8725e6217d3ded
Ryan Christian <christianr@cardiff.ac.uk>
2018-05-21 18:04:05 +0100

    Fixed sharedPref for intro, and other small changes

--- app/src/main/java/com/assignment/spotabee/database/DatabaseInitializer.java
+++ app/src/main/java/com/assignment/spotabee/database/DatabaseInitializer.java
@@ -13,7 +13,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Initializes the database in a singleton format.
+ * Populates the data with test information.
  */
 public class DatabaseInitializer {
 
